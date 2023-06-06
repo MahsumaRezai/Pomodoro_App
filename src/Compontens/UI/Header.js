0ae50d70c7064bf5f3a9d3437d6aa5e8a7150eed
Masoumeh Rezai <you@example.com>
2023-06-07 01:20:23 +0430

    work on haeder section

---
+++ src/Compontens/UI/Header.js
@@ -0,0 +1,13 @@
+const Header = (porps) => {
+    return (
+        <div>
+            <div>
+                <div><h1>Pomofocus</h1></div>
+                <div></div>
+            </div>
+
+        </div>
+    )
+
+}
+export default Header
