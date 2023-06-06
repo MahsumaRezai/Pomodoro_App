29285758f69ed611812b96bee19e96b766d968e3
Masoumeh Rezai <you@example.com>
2023-06-07 02:20:25 +0430

    work on cart section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
+import Cart from "./Compontens/UI/Cart";
 import Header from "./Compontens/UI/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
-
-
-
-
-
+      <Cart/>
     </div>
   )
 
