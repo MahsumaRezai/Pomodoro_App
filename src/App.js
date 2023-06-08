ec788777888fd7215877485a3a915e590faababf
Masoumeh Rezai <you@example.com>
2023-06-08 23:57:50 +0430

    work on add Task section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import Header from "./Compontens/UI/Header";
 import Addtime from "./Compontens/Card/Addtime";
-import Task from "./Compontens/UI/Taks/Task";
+import Form from './Compontens/UI/Taks/Form';
+import List from './Compontens/UI/Taks/List'
 
 
 const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <List items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+
   return (
     <div>
       <Header />
       <Addtime />
-      <Task/>
+      <section id="goal-form">
+        <Form onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+
+      </section>
     </div>
   )
 
