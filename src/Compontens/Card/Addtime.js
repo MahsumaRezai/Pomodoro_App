9b05cfe47cea52e3de7f021e7903961e2d35f00e
Masoumeh Rezai <you@example.com>
2023-06-10 08:15:41 +0430

    work on clock section

--- src/Compontens/Card/Addtime.js
+++ src/Compontens/Card/Addtime.js
@@ -1,26 +1,38 @@
-
 import Card from '../UI/Card';
 import classes from './Addtime.module.css'
 import ButtonLogn from './ButtonLogn';
 import ButtonPomdor from './ButtonPomdor';
 import ButtonShort from './ButtonShort';
 const Addtime = (props) => {
-   
+    const startHandler = () => {
+        const data = new Date();
+        document.getElementById("start").innerText = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+
+    }
+    setInterval(startHandler, 1000);
+    const endHanlder = () => {
+       return clearTimeout(startHandler)
+
+    }
+
+
+
+
     return (
 
         <Card >
             <form className={classes.content}>
                 <div className={classes.buttons}>
                     <ButtonPomdor>Pomodoro</ButtonPomdor>
-                    <ButtonShort>Short Break</ButtonShort>
+                    <ButtonShort >Short Break</ButtonShort>
                     <ButtonLogn>Long Break</ButtonLogn>
                 </div>
                 <div>
-                    <div className={classes.time} id='title'>0</div>
+                    <div className={classes.time} id="start">0</div>
                 </div>
                 <div className={classes.btns}>
-                    <button className={classes.btn} >START</button>
-                    <button className={classes.btn} >POUSE</button>
+                    <button className={classes.btn} onClik={startHandler} >START</button>
+                    <button className={classes.btn}onClick={endHanlder} >POUSE</button>
 
                 </div>
 
